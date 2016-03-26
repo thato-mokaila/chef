280d34a6c5de763bc7ade51c40715c3245d9898b
thato-mokaila <thato@smartpixel.co.za>
2016-03-26 12:12:42 +0200

    testing cookbook

--- cookbooks/ibm-mq/recipes/default.rb
+++ cookbooks/ibm-mq/recipes/default.rb
@@ -12,6 +12,15 @@ script "prepare_mq_environment" do
   cwd "#{node['mq']['install_script_dir']}"
   code <<-EOH
     #insert bash script
-    setenv.sh
+	export WMQ_INSTALL_DIR=/opt/mqm
+	export LD_LIBRARY_PATH=$WMQ_INSTALL_DIR/java/lib64
+	export JAVA_HOME=$WMQ_INSTALL_DIR/java/jre64/jre
+	export PATH=$PATH:$WMQ_INSTALL_DIR/bin:$JAVA_HOME/bin
+	export QM=QM1
+	export PORT=1414
+	export REQUESTQ=REQUEST_Q
+	export REPLYQ=REPLY_Q
+	export INSTALL_USER=developer
+	export MQ_EXLPODED_DIR=MQServer
   EOH
 end
