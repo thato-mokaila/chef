c043c9f2a8c2fc6bb85c4d1f1685df3249416383
thato-mokaila <thato@smartpixel.co.za>
2016-03-31 19:14:36 +0200

    update init scripts

--- init.sh
+++ init.sh
@@ -9,28 +9,21 @@ chec_solo=/usr/bin/chef-solo
 
 # Are we on a vanilla system?
 if ! type "$chec_solo" > /dev/null; then
-
 	# update system to latest
 	yum -y update
-
-	yum -y install gcc ruby-devel rubygems git
-
-	# Chef Client
+	yum -y install gcc ruby-devel rubygems git tree
+	# chef client
 	curl -L https://www.opscode.com/chef/install.sh | sudo bash
-
 fi &&
 
 # goto working directory
-cd /var && mkdir -p /tmp/mq_install/scripts
+cd /var 
 
 # check out cookbooks into working directory
 git clone https://github.com/thato-mokaila/chef.git
 chmod -R 755 ./chef && cd ./chef/ 
-cp -r ./mq/scripts/ /tmp/mq_install/scripts
-cp ./mq/scripts/functions.sh /usr/local/sbin
-cp ./mq/scripts/mqconfig.sh /usr/local/sbin
-cp ./mq/config/qm.ini.tmp /tmp/mq_install
-cp ./mq/config/config.mqsc /tmp/mq_install
+cp ./chef/mq/config/qm.ini.tmp /tmp/mq_install
+cp ./chef/mq/config/config.mqsc /tmp/mq_install
 
 # run chef to prep the system
 "$chec_solo" -c solo.rb -j node.json
