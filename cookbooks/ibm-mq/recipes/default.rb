7c8939c78a851a3b71633180f4436ef90aea7c40
thato-mokaila <thato@smartpixel.co.za>
2016-03-31 21:11:22 +0200

    inititlaining using provision shell

--- cookbooks/ibm-mq/recipes/default.rb
+++ cookbooks/ibm-mq/recipes/default.rb
@@ -12,15 +12,20 @@ ENV['JAVA_HOME'] = "#{node[:MQ][:WMQ_INSTALL_DIR]}/java/jre64/jre"
 ENV['PATH'] = "#{ENV['PATH']}:#{node[:MQ][:WMQ_INSTALL_DIR]}/bin:#{ENV['JAVA_HOME']}/bin"
 
 # prep mq environment
-bash "print_mq_environment" do
-  code <<-EOH
+bash "prepare_mq_environment" do
+    code <<-EOH
+
 	echo "#**********************************************************"
-	echo "# Preparing MQ Environment 								 "
+    echo "# Environment Variables 								     "
 	echo "#**********************************************************"
+
 	echo "#**********************************************************"
-	echo $LD_LIBRARY_PATH	
-    echo $JAVA_HOME
-    echo $PATH
+    export LD_LIBRARY_PATH=#{node[:MQ][:WMQ_INSTALL_DIR]}/java/lib64
+    export JAVA_HOME=#{node[:MQ][:WMQ_INSTALL_DIR]}/java/jre64/jre
+    export PATH=#{ENV['PATH']}:#{node[:MQ][:WMQ_INSTALL_DIR]}/bin:#{ENV['JAVA_HOME']}/bin
 	echo "#**********************************************************"
+
+	echo "# Finished exporting variables..."
+    
 EOH
 end
