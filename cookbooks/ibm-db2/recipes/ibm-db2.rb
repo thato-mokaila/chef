b61d41bf335ff2e043b6bf9e351571b141dbd6f4
thato-mokaila <thato@smartpixel.co.za>
2016-04-20 09:03:06 +0200

    update db2 recipe

--- cookbooks/ibm-db2/recipes/ibm-db2.rb
+++ cookbooks/ibm-db2/recipes/ibm-db2.rb
@@ -7,13 +7,50 @@
 # All rights reserved - Do Not Redistribute
 #
 
+# create group 'db2sdfe1'
+group db2sdfe1
+
+# create group 'db2admin'
+group db2admin
+
+# add db2sdfe1 user
+user db2sdfe1 do
+    comment 'A user required to run DB2'
+  	group 'db2sdfe1'
+    home '/home/db2sdfe1'
+  	system true
+  	shell '/bin/bash'
+end
+
+# add db2admin user
+user db2admin do
+    comment 'A user required to run DB2'
+  	group 'db2admin'
+    home '/home/db2admin'
+  	system true
+  	shell '/bin/bash'
+end
+
+# update kernel parameters
+bash "update_kernel_parameters" do
+code <<-EOH
+sysctl -w kernel.sem=250 256000 32 4096
+sysctl -w kernel.msgmnb=65536
+sysctl -w kernel.msgmax=65536
+sysctl -w kernel.msgmni=16384
+sysctl -w kernel.shmmni=4096
+sysctl -w kernel.shmall=8388608
+sysctl -w kernel.shmmax=17179869184
+EOH
+end
+
 # install db2
 bash "install_db2_using_response_file" do
     code <<-EOH
 
-	echo "#**********************************************************"
-    echo "# installing DB2 Express C v10.5 ...					     "
-	echo "#**********************************************************"
+echo "#**********************************************************"
+echo "# installing DB2 Express C v10.5 ...					     "
+echo "#**********************************************************"
 
 cd /tmp/db2_install/
 tar -xvzf ./v10.5_linuxx64_expc.tar.gz
